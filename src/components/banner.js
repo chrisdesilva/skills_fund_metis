46398ee187d42ce405dbd0991db9d691fbac9239
Chris DeSilva <chris.a.desilva@gmail.com>
2019-07-19 13:27:10 -0500

    Fix banner button positioning

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,7 +4,7 @@ import Button from "./button"
 
 const Banner = props => (
     <div>
-        <div className="bg-cover text-white flex flex-col justify-center items-center relative z-0" style={{backgroundImage: `url(${banner})`, height: '25rem'}} id="banner">
+        <div className="bg-cover text-white flex flex-col justify-center items-center relative z-0 pb-4" style={{backgroundImage: `url(${banner})`, height: '25rem'}} id="banner">
 
             <h2 className="text-3xl lg:text-4xl font-normal text-center mb-5 mt-24">Experience Immersive Data Science Instruction at Metis</h2>
             <h3 className="text-base lg:text-xl font-light text-center">Know exactly what you'll pay with Skills Fund</h3>
@@ -16,7 +16,7 @@ const Banner = props => (
                     onClick={props.howItWorksOnClick}
                 />
                 <Button
-                    buttonClassName="uppercase bg-tertiary p-3 mb-8 lg:mb-0 lg:ml-4 w-40 rounded-full shadow-lg text-white"
+                    buttonClassName="uppercase bg-tertiary p-3 lg:mb-0 lg:ml-4 w-40 rounded-full shadow-lg text-white"
                     divClassName="flex justify-center mt-5"
                     text="apply now"
                     onClick={props.applyNowOnClick}
