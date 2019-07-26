895ff1278d4d03de68ea4eea8e171174fb61b1a8
Chris DeSilva <chris.a.desilva@gmail.com>
2019-07-26 12:12:23 -0500

    Add custom Hubspot form

--- src/components/loanapp.js
+++ src/components/loanapp.js
@@ -1,27 +1,29 @@
 import React, { useState } from 'react'
-import HubspotForm from 'react-hubspot-form'
+// import HubspotForm from 'react-hubspot-form'
 import ReactGA from 'react-ga'
 import marching from '../images/PeopleMarchColor.png'
 
 const LoanApp = React.forwardRef((props, ref) => {
 
-    // const [email, setEmail] = useState('')
+    const [email, setEmail] = useState('')
 
-    // const handleChange = e => {
-    //     setEmail(e.target.value)
-    // }
+    const handleChange = e => {
+        setEmail(e.target.value)
+    }
 
-    // const redirectLoanApp = () => {
-    //     window.open("https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SKMETA18", "_blank", "noopener")
-    // }
 
-    const trackGoogleAnalyticsEvent = () => {
-        ReactGA.event({
-            category: 'Apply Now Button',
-            action: 'click',
-            label: 'clicked on loan app input field or button'
-          })
-        // redirectLoanApp()
+
+    const redirectLoanApp = e => {
+        window.open("https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SKMETA19", "_blank", "noopener noreferrer")
+    };
+
+    const trackGoogleAnalyticsEvent = e => {
+            ReactGA.event({
+                category: 'Apply Now Button',
+                action: 'click',
+                label: 'submitted loan application'
+              })
+                redirectLoanApp()
     }
 
     
@@ -29,9 +31,9 @@ const LoanApp = React.forwardRef((props, ref) => {
         <div ref={ref} className="flex flex-col items-center justify-center py-8 mx-2 lg:mx-10 rounded shadow-xl">
             <h3 className="text-center">Apply for Metis Tuition and Cost of Living Funding</h3>
             <div className="flex justify-center">
-                <img onClick={trackGoogleAnalyticsEvent} className="w-auto" src={marching} alt="People marching and carrying flags" />
+                <img className="w-auto" src={marching} alt="People marching and carrying flags" />
             </div>
-            <div onClick={trackGoogleAnalyticsEvent}>
+            {/* <div onClick={trackGoogleAnalyticsEvent}>
                 <HubspotForm
                     portalId='3871135'
                     formId='373d1cf3-11e4-4798-be11-65ad5b5619a6'
@@ -39,15 +41,18 @@ const LoanApp = React.forwardRef((props, ref) => {
                     submitButtonClass='loanAppSubmitBtn'
                     cssClass='program-apply'
                 />
-            </div>
-            {/* <form className="metis_apply_now_GA program-apply flex flex-col items-center">
+            </div> */}
+            <form className="metis_apply_now_GA program-apply flex flex-col items-center" method="post" action="https://forms.hubspot.com/uploads/form/v2/3871135/0abd7292-985c-43fe-8480-0759a6cba99e" onSubmit={trackGoogleAnalyticsEvent}>
                 <label htmlFor="email">Email address</label>
                 <input className="applyNowInput" type="email" name="email" placeholder="Enter your email address" onChange={handleChange} value={email} required />
-                <div className="inline-block" onClick={trackGoogleAnalyticsEvent}>
-                    <input className="w-40" value="APPLY NOW" id="loanAppSubmitBtn" type="submit"/>
-                </div>
-                <p className="mt-3 text-xs italic">Please note: clicking Apply Now will redirect this page to your application</p>
-            </form> */}
+                <input type="hidden" name="Stakeholder Type" value="Student"/>
+                <input type="hidden" name="Program Name" value="Data Science Bootcamp"/>
+                <input type="hidden" name="School" value="Metis"/>
+                <input type="hidden" name="Student Loan Application Status" value="BLA Click Email Submitted"/>
+                <input type="hidden" name="Clicked Begin Loan Application BLA" value="BLA Click"/>
+                <input className="w-40" value="APPLY NOW" id="loanAppSubmitBtn" type="submit"/>
+                <p className="mt-3 text-xs italic">Please note: clicking Apply Now will open your loan application in a new tab</p>
+            </form>
             <div className="px-8 text-sm">
                 <p className="text-center mb-12">If you are a cosigner, begin the addendum now by clicking <a className="text-primary" href="https://sf.privateloan.studentloan.org/Cosigner.do?execution=e1s1" rel="noreferrer noopener" target="_blank">here</a>.</p>
                 <p><strong>Before you begin, please read these important notes:</strong></p>
