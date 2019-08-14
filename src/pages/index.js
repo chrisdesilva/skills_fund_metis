ce7d9c20ec82a673a04e215ca9d71421ca22ae0e
Chris DeSilva <chris.a.desilva@gmail.com>
2019-08-14 12:43:13 -0500

    Convert index.js to class component for Facebook Pixel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import Homepage from '../components/homepage'
 import ReactGA from 'react-ga'
-import ReactPixel from 'react-facebook-pixel'
-
-const options= {
-  autoConfig: true,
-  debug: false
-}
-ReactPixel.init('928181257515785', options)
 
 const trackingId = "UA-68312423-5"
 ReactGA.initialize(trackingId, {
@@ -18,22 +11,30 @@ ReactGA.initialize(trackingId, {
   }
 })
 
-const IndexPage = () => {
-  return (
-    <div>
-      <form name="metis_contact" data-netlify="true" netlify-honeypot="bot-field" hidden>
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <input type="text" name="school"/>
-        <input type="button" name="identity" />
-        <input type="button" name="identity" />
-        <input type="button" name="identity" />
-        <input type="text" name="otherDescription" />
-        <textarea name="comments" />
-      </form>
-      <Homepage />
-    </div>
-  )
+class IndexPage extends React.Component {
+
+  componentDidMount() {
+    const ReactPixel =  require('react-facebook-pixel');
+    ReactPixel.init('928181257515785');
+  }
+
+  render() {
+    return (
+      <div>
+        <form name="metis_contact" data-netlify="true" netlify-honeypot="bot-field" hidden>
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <input type="text" name="school"/>
+          <input type="button" name="identity" />
+          <input type="button" name="identity" />
+          <input type="button" name="identity" />
+          <input type="text" name="otherDescription" />
+          <textarea name="comments" />
+        </form>
+        <Homepage />
+      </div>
+    )
+  }
 }
 
 export default IndexPage
